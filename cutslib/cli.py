d19547ee4a326fa28425ffa9dae68499160c2ade
Yilun Guan <zoom.aaron@gmail.com>
2019-11-30 15:58:17 -0500

    Add more edge cases in cli:main

--- cutslib/cli.py
+++ cutslib/cli.py
@@ -4,13 +4,25 @@ import sys, os
 
 def main():
     argv = sys.argv[1:]
+    if len(argv) < 2:
+        print("Too few arguments!")
+        print("Format: cuts recipe method arg1 arg2 ...")
+        sys.exit(-1)
     recipe = argv[0]
     method = argv[1]
-    arguments = argv[2:]
+
+    has_arguments = False
+    if len(argv) > 2:
+        has_arguments = True
+        arguments = argv[2:]
+
     mod_name = "cutslib.recipes.%s" % recipe
     print("Recipe: %s" % mod_name)
     mod = importlib.import_module(mod_name)
-    ret = getattr(mod, method)(*arguments)
+    if has_arguments:
+        ret = getattr(mod, method)(*arguments)
+    else:
+        ret = getattr(mod, method)()
 
     if isinstance(ret, list):
         print("======================")
