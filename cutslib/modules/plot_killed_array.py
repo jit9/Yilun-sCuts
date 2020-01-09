daf0b96795f43e5e3dfcc3a3f379c20994f7b6f9
Yilun Guan <zoom.aaron@gmail.com>
2020-01-09 09:40:12 -0500

    plot_killed_array: add options to adjust plot range

--- cutslib/modules/plot_killed_array.py
+++ cutslib/modules/plot_killed_array.py
@@ -1,16 +1,22 @@
 """This module creates the killedbyplot that shows how many detectors
-pass each cut criteria"""
+pass each cut criteria
+
+Options:
+    limit_{l,h}: lower and upper limit for the plot"""
 
 from cutslib.pathologyReport import pathoReport
 
 def init(config):
-    pass
+    global limit_l, limit_h
+    limit_l = config.getint("limit_l", 400)
+    limit_h = config.getint("limit_h", 800)
 
 def run(p):
+    global limit_l, limit_h
     pr = pathoReport(filename=str(p.i.db))
     outfile = p.o.patho.root+"/killedbyplot.png"
     print("Saving plot: %s" % outfile)
     pr.killedbyplot(filename=outfile)
-    outfile = p.o.patho.root+"/killedbyplot_400_800.png"
+    outfile = p.o.patho.root+"/killedbyplot_%d_%d.png" % (limit_l, limit_h)
     print("Saving plot: %s" % outfile)
-    pr.killedbyplot(filename=outfile, dets_lim=[400,800])
+    pr.killedbyplot(filename=outfile, dets_lim=[limit_l,limit_h])
