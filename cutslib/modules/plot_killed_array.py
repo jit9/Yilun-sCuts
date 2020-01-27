6637a9b1397d17ac220c40edb820d244534eb658
Yilun Guan <zoom.aaron@gmail.com>
2020-01-27 16:07:06 -0500

    plot_killed_array: add option type

--- cutslib/modules/plot_killed_array.py
+++ cutslib/modules/plot_killed_array.py
@@ -7,16 +7,14 @@ Options:
 from cutslib.pathologyReport import pathoReport
 
 def init(config):
-    global limit_l, limit_h
+    global limit_l, limit_h, type
     limit_l = config.getint("limit_l", 400)
     limit_h = config.getint("limit_h", 800)
+    type = config.get("type", "violin")
 
 def run(p):
-    global limit_l, limit_h
+    global limit_l, limit_h, type
     pr = pathoReport(filename=str(p.i.db))
     outfile = p.o.patho.root+"/killedbyplot.png"
     print("Saving plot: %s" % outfile)
-    pr.killedbyplot(filename=outfile)
-    outfile = p.o.patho.root+"/killedbyplot_%d_%d.png" % (limit_l, limit_h)
-    print("Saving plot: %s" % outfile)
-    pr.killedbyplot(filename=outfile, dets_lim=[limit_l,limit_h])
+    pr.killedbyplot(filename=outfile, dets_lim=[limit_l,limit_h], type=type)
