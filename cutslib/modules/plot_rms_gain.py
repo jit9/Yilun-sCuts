0f8651698bf665655e2dde8eef7052f02ab44667
Yilun Guan <zoom.aaron@gmail.com>
2020-02-05 15:50:06 -0500

    plot_rms_gain: add two more histograms

--- cutslib/modules/plot_rms_gain.py
+++ cutslib/modules/plot_rms_gain.py
@@ -26,6 +26,7 @@ def run(p):
     data['MFELive'] *= input_gain
     data['DELive'] *= input_gain
     data['jumpLive'] *= input_gain
+    import pdb;pdb.set_trace()
     # make 2d histogram
     # x: rms, y: input gain
     # get bin edges
@@ -37,18 +38,35 @@ def run(p):
     gain = input_gain[sel]
     H, _, __ = np.histogram2d(rms, gain, bins=(x_edges,y_edges))
     # make individual hists for reference
+    # hist for gain
     plt.hist(gain, bins=ybins)
     plt.xlabel("gain")
     outfile = op.join(p.o.root, "gain_hist.png")
     print("Saving plot: %s" % outfile)
     plt.savefig(outfile)
     plt.clf()
+    # hist for rms
     plt.hist(rms, bins=xbins)
     plt.xlabel("rms")
     outfile = op.join(p.o.root, 'rms_hist.png')
     print("Saving plot: %s" % outfile)
     plt.savefig(outfile)
     plt.clf()
+    # hist for bias-step
+    plt.hist(data['resp'][sel], bins=xbins)
+    plt.xlabel("bias-step")
+    outfile = op.join(p.o.root, 'bs_hist.png')
+    print("Saving plot: %s" % outfile)
+    plt.savefig(outfile)
+    plt.clf()
+    # hist for flatfield
+    plt.hist(data['ff'], bins=xbins)
+    plt.xlabel("flatfield")
+    outfile = op.join(p.o.root, 'ff_hist.png')
+    print("Saving plot: %s" % outfile)
+    plt.savefig(outfile)
+    plt.clf()
+    # start to produce the 2d histogram
     fig, ax = plt.subplots()
     mesh = ax.pcolormesh(X,Y,H, cmap="hot")
     plt.colorbar(mesh)
