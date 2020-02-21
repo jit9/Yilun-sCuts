d61d73bfb16d80bc2ecc207295a7d1043d3dd60a
Yilun Guan <zoom.aaron@gmail.com>
2020-02-21 11:23:34 -0500

    environ: add environment variable support

---
+++ cutslib/environ.py
@@ -0,0 +1,9 @@
+"""Parse the environment variables for the use of the entire
+package"""
+
+import os
+
+CUTS_USER = os.environ.get("CUTS_USER", "yilung")
+CUTS_DIR = os.environ.get("CUTS_DIR","/scratch/gpfs/yilung/cuts/")
+CUTS_PYENV = os.environ.get("CUTS_PYENV", "myenv")
+CUTS_DEPOT = os.environ.get("CUTS_DEPOT", "/scratch/gpfs/yilung/depot/")
