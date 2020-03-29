bbbfae7eb6f40deebc84781313ae5b9c3425117b
Yilun Guan <zoom.aaron@gmail.com>
2020-03-29 10:25:50 -0400

    readme: add text on environment variables

--- cutslib/environ.py
+++ cutslib/environ.py
@@ -3,7 +3,7 @@ package"""
 
 import os
 
-CUTS_USER = os.environ.get("CUTS_USER", "yilung")
+CUTS_USER = os.environ.get("USER", "yilung")
 CUTS_DIR = os.environ.get("CUTS_DIR","/scratch/gpfs/yilung/cuts/")
 CUTS_PYENV = os.environ.get("CUTS_PYENV", "myenv")
 CUTS_DEPOT = os.environ.get("CUTS_DEPOT", "/scratch/gpfs/yilung/depot/")
