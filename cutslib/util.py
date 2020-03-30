2528a7566cc51956856b77ab949f22d75711d47b
Yilun Guan <zoom.aaron@gmail.com>
2020-03-30 15:40:17 -0400

    util: copy some utils function from pixell here to reduce dependency

--- cutslib/util.py
+++ cutslib/util.py
@@ -1,6 +1,6 @@
 """This script contains common utility function for all other scripts"""
 
-import os, pickle
+import os, pickle, numpy as np
 import moby2
 
 def tag_to_afsv(tag, ar=True):
@@ -73,3 +73,54 @@ def to_pa(array):
 
 def to_ar(array):
     return array.lower().replace('pa','ar')
+
+# functions copied from pixell.utils to reduce dependency
+def cumsum(a, endpoint=False):
+	"""As numpy.cumsum for a 1d array a, but starts from 0. If endpoint is True, the result
+	will have one more element than the input, and the last element will be the sum of the
+	array. Otherwise (the default), it will have the same length as the array, and the last
+	element will be the sum of the first n-1 elements."""
+	res = np.concatenate([[0],np.cumsum(a)])
+	return res if endpoint else res[:-1]
+
+def moveaxis(a, o, n):
+	if o < 0: o = o+a.ndim
+	if n < 0: n = n+a.ndim
+	if n <= o: return np.rollaxis(a, o, n)
+	else: return np.rollaxis(a, o, n+1)
+
+def allgather(a, comm):
+	"""Convenience wrapper for Allgather that returns the result
+	rather than needing an output argument."""
+	a   = np.asarray(a)
+	res = np.zeros((comm.size,)+a.shape,dtype=a.dtype)
+	if np.issubdtype(a.dtype, np.string_):
+		comm.Allgather(a.view(dtype=np.uint8), res.view(dtype=np.uint8))
+	else:
+		comm.Allgather(a, res)
+	return res
+
+def allgatherv(a, comm, axis=0):
+	"""Perform an mpi allgatherv along the specified axis of the array
+	a, returning an array with the individual process arrays concatenated
+	along that dimension. For example gatherv([[1,2]],comm) on one task
+	and gatherv([[3,4],[5,6]],comm) on another task results in
+	[[1,2],[3,4],[5,6]] for both tasks."""
+	a  = np.asarray(a)
+	fa = moveaxis(a, axis, 0)
+	# mpi4py doesn't handle all types. But why not just do this
+	# for everything?
+	must_fix = np.issubdtype(a.dtype, np.str_) or a.dtype == bool
+	if must_fix:
+		fa = fa.view(dtype=np.uint8)
+	ra = fa.reshape(fa.shape[0],-1) if fa.size > 0 else fa.reshape(0,np.product(fa.shape[1:],dtype=int))
+	N  = ra.shape[1]
+	n  = allgather([len(ra)],comm)
+	o  = cumsum(n)
+	rb = np.zeros((np.sum(n),N),dtype=ra.dtype)
+	comm.Allgatherv(ra, (rb, (n*N,o*N)))
+	fb = rb.reshape((rb.shape[0],)+fa.shape[1:])
+	# Restore original data type
+	if must_fix:
+		fb = fb.view(dtype=a.dtype)
+	return moveaxis(fb, 0, axis)
