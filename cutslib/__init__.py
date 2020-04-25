234cc0981189fd1a50a3880435c8e8d97048a6ae
Yilun Guan <zoom.aaron@gmail.com>
2020-04-24 23:17:45 -0400

    recipes/run: add errors_tod recipe to grep tod with given error

--- cutslib/__init__.py
+++ cutslib/__init__.py
@@ -1,2 +1,6 @@
 from .depot import SharedDepot, Depot
 from .catalog import Catalog
+
+# short cut for long import
+from moby2.util.database import TODList
+from moby2.util import MobyDict
--- cutslib/recipes/run.py
+++ cutslib/recipes/run.py
@@ -7,54 +7,6 @@ from cutslib.environ import CUTS_DIR, CUTS_PYENV
 # interactive action #
 ######################
 
-def submit(cutparam):
-    from moby2.util import MobyDict
-
-    # load base parameters
-    par = MobyDict.from_file(cutparam)
-
-    # runtime parameters
-    tpn = par.get("tpn", 20)  # task per node
-    nnode = par.get("nnode", 1)
-    jobname = par.get("jobname", "get_cuts")
-    nproc = tpn * nnode
-
-    # output parameters
-    basedir = os.path.dirname(os.path.abspath(cutparam))
-    outdir = os.path.join(basedir, par["outdir"])
-    runtime = par.get("runtime")
-    qos = par.get("qos")
-    nomp = par.get("nomp", 1)
-    # partition = par.get("partition", "serial")
-    # total task per node including nomp
-    # note there is a maximum of 64 logical cores in della
-    ttpn = tpn * nomp
-
-    # find list of tods to process
-    if not(os.path.isdir(outdir)): os.makedirs(outdir)
-
-    # submit one slurm job on each node, so loop over node here
-    for n in range(nnode):
-        f = open( '%s/submitjob.sh.%d' % (outdir, n), 'w' )
-        f.write( '#!/bin/sh\n' )
-        f.write( '#SBATCH -N 1\n')
-        f.write( '#SBATCH --ntasks-per-node=40\n')  # default for della
-        f.write( '#SBATCH -J %s%d\n' % (jobname,n))
-        f.write( '#SBATCH -t %s\n' % runtime )
-        f.write( '#SBATCH --qos %s\n' % qos )
-        # f.write( '#SBATCH --partition %s\n' % partition)
-        f.write( '#SBATCH --output=%s/slurmjob.log.%d\n' % (outdir, n))
-        f.write( 'module load gcc\n' )
-        f.write( 'module load openmpi\n' )
-        f.write( 'source activate %s\n' % CUTS_PYENV )  #FIXME
-        f.write( 'cd %s\n' % basedir)
-        start, end = n*tpn, (n+1)*tpn
-        for i in range(start, end):
-            f.write('OMP_NUM_THREADS=%d run_cuts -n %d --index %d -f %s & sleep 1\n' % (nomp, nproc, i, cutparam))
-        f.write('wait\n')
-        f.close()
-        os.system("sbatch %s/submitjob.sh.%d\n" % (outdir, n))
-
 def list(all=None):
     """List all the latest cuts parameters and the status of it"""
     # get cuts tags information
@@ -103,6 +55,11 @@ def logs(cpar_path):
     run_dir = os.path.join(basedir, f"run_v{ver}")
     os.system(f"tail -f {run_dir}/slurmjob.log.*")
 
+def errors_tod(cpar_path, match):
+    ver = cpar_path.split(".par")[0][-1]  # FIXME
+    basedir = os.path.dirname(os.path.abspath(cpar_path))
+    run_dir = os.path.join(basedir, f"run_v{ver}")
+    os.system("cat %s/error_list.txt | grep -i %s | awk '{print $2}' | sort | uniq" % (run_dir, match))
 
 ####################
 # utility function #
