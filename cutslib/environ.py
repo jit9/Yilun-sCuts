74f08ed46a4da7146592a123226806df747a360d
Yilun Guan <zoom.aaron@gmail.com>
2020-04-25 21:56:09 -0400

    environ: update default environment variables

--- cutslib/environ.py
+++ cutslib/environ.py
@@ -4,6 +4,6 @@ package"""
 import os
 
 CUTS_USER = os.environ.get("USER", "yilung")
-CUTS_DIR = os.environ.get("CUTS_DIR","/scratch/gpfs/yilung/cuts/")
+CUTS_DIR = os.environ.get("CUTS_DIR","/projects/ACT/yilung/cuts/")
 CUTS_PYENV = os.environ.get("CUTS_PYENV", "myenv")
-CUTS_DEPOT = os.environ.get("CUTS_DEPOT", "/scratch/gpfs/yilung/depot/")
+CUTS_DEPOT = os.environ.get("CUTS_DEPOT", "/projects/ACT/yilung/depot/")
