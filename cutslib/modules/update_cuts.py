7b1d5f739a435a4358906edf77555b031932d66d
Yilun Guan <zoom.aaron@gmail.com>
2020-04-27 16:58:31 -0400

    update_cuts: add module to do similar things to update_crit

---
+++ cutslib/modules/update_cuts.py
@@ -0,0 +1,36 @@
+"""Update cuts, just like the todloop based update_crit but
+is more about manually running in an interactive node for a quick
+run"""
+
+import os.path as op, os
+import moby2
+from cutslib import TODList
+from cutslib.pathologies_tools import reportPathologies
+
+
+class Module:
+    def __init__(self, config):
+        # whether we want to overwrite existing db file
+        self.overwrite = config.getboolean('overwrite', False)
+
+    def run(self, p):
+        overwrite = self.overwrite
+        cpar = moby2.util.MobyDict.from_file(p.i.cutparam)
+        # check whether db file exists
+        # if op.exists(p.i.db):
+        #     if not overwrite:
+        #         raise RuntimeError("db file exists!")
+        #     else:  # remove it
+        #         os.remove(p.i.db)
+        # load tod source list
+        source_scans = cpar.get("source_scans")
+        obsnames = TODList.from_file(source_scans)
+        # initialize reportPathologies object to collect results
+        report = reportPathologies(p.i.cutparam)
+        report.depot_file += f".{p.rank}"
+        for i in range(len(obsnames)):
+            obs = obsnames[i]
+            print(f"f{p.rank:2d} {i:5d}/{len(obsnames)}: {obs}")
+            # load data without actually loading it
+            report.appendResult(obs)
+        p.comm.Barrier()
