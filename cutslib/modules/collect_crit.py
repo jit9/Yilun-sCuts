246dd5936be2a756af4cb8956c24f306fb88b76c
Yilun Guan <zoom.aaron@gmail.com>
2020-04-26 21:58:40 -0400

    collect_crit: add more fields in it so collect_sel is unnecessary

--- cutslib/modules/collect_crit.py
+++ cutslib/modules/collect_crit.py
@@ -1,11 +1,12 @@
 """This is a wrapper to the collectSeasonCrit script in moby2. It is
 wrapped in this framework for convenience"""
-import os
+import os, os.path as op
 import moby2
 import pickle, numpy as np, sys, os
 from moby2.util.database import TODList
 from cutslib.pathologies_tools import fix_tod_length, get_pwv
-from cutslib.pathologies import Pathologies
+from cutslib import Catalog
+from cutslib.pathologies import get_pathologies, Pathologies
 from cutslib import util
 
 class Module:
@@ -14,109 +15,111 @@ class Module:
 
     def run(self, p):
         limit = self.limit
-
+        all_keys = ["gainLive", "corrLive", "normLive", "rmsLive",
+                    "kurtLive", "skewLive", "MFELive", "DELive", "jumpLive"]
         # read parameters
-        params = moby2.util.MobyDict.from_file(p.i.cutparam)
-        depot = moby2.util.Depot(params.get('depot'))
-        cutParams = moby2.util.MobyDict.from_file(p.i.cutParam)
-        pathop = cutParams['pathologyParams']
-
-        # get tod list
-        obsnames = TODList.from_file(params.get("source_scans"))
+        cpar = moby2.util.MobyDict.from_file(p.i.cutparam)
         depot_file = p.i.db
-        if os.path.isfile(depot_file):
-            done = TODList.from_file(depot_file)
-            undone = obsnames - done
-            obsnames -= undone
+        source_scans = cpar.get("source_scans")
+        catalog = Catalog()
+        # narrow down to the source list
+        catalog.narrow_down(source_scans)
+        base_arr = catalog.data[['tod_name','loading']].values
 
-        print("Collecting Criteria for %d files"%len(obsnames))
-        tods = []
-        sel = []
-        psel = []
-        scanf = []
-        resp = []
-        respSel = []
-        cal = []
-        ctimes = []
-        criteria = {}
-        alt = []
-        if params.get("keys") is None:
-            keys = ["corrLive", "DELive", "MFELive",
-                    "rmsLive", "skewLive", "kurtLive",
-                    "normLive", "gainLive", "jumpLive"]
-        else:
-            keys = params.get("keys")
-
+        depot = moby2.util.Depot(p.depot)
+        res = {
+            'tods': [],
+            'sel': [],
+            'psel': [],
+            'scanf': [],
+            'resp': [],
+            'resp_sel': [],
+            'cal': [],
+            'ctimes': [],
+            'pwv': [],
+            'alt': [],
+            'tod_sel': []
+        }
+        for key in all_keys:
+            res[key] = []
+            res[f"{key}_sel"] = []
         if limit:
-            obsnames = obsnames[:limit]
-
-        n = 0
-        N = len(obsnames)
-        for obs_i in range(p.rank, len(obsnames), p.size):
-            obs = obsnames[obs_i]
-            print("%2d %s: %5d/%5d"%(p.rank, obs.split("/")[-1], obs_i, N))
-            n += 1
+            base_arr = base_arr[:limit,:]
+        n_tot = base_arr.shape[0]
+        print(f"Collecting Criteria for {n_tot} files")
+        for i in range(p.rank, n_tot, p.size):
+            obs = base_arr[i,0]
+            pwv = base_arr[i,1]
+            print(f"{p.rank:3d} {obs} {i:>5d}/{n_tot:>5d} {pwv:.2f}")
+            # try loading, if failed add it to tod_sel
             try:
-                tod = moby2.scripting.get_tod({"filename":obs, "read_data":False})
+                tod = moby2.scripting.get_tod({"filename": obs,
+                                               "read_data": False})
             except:
-                print("Failed")
+                # failed to process
+                res['tod_sel'].append(False)
                 continue
-            if os.path.isfile(depot.get_full_path(Pathologies, tod=tod, tag=params['tag_patho'])) and \
-               os.path.isfile(depot.get_full_path(moby2.TODCuts, tod=tod, tag=params["tag_out"])) and \
-               os.path.isfile(depot.get_full_path(moby2.Calibration, tod=tod, tag=params["tag_cal"])):
-                calo = depot.read_object(moby2.Calibration, tod=tod, tag=params["tag_cal"])
-                if len(calo.cal)==0:
-                    print("No calibration available")
-                    continue
-                pa = depot.read_object(Pathologies, tod=tod, tag=params['tag_patho'],
-                                       options = {"tod":tod, "params":pathop},
-                                       structure = params.get("structure"))
-                for crit in keys:
-                    if "values" in pa.crit[crit]:
-                        criteria.setdefault(crit,[]).append( pa.crit[crit]["values"] )
-                fix_tod_length(tod, pa.offsets)
-                cuts = depot.read_object(moby2.TODCuts, tod=tod, tag=params["tag_out"])
-                re, ff, _, re_sel, _, stable = pa.getpWCalibration()
-                # get final cut
-                lsel = np.zeros(pa.ndet)
-                lsel[cuts.get_uncut()] = True
-                sel.append(lsel)
+            if os.path.isfile(depot.get_full_path(Pathologies, tod=tod, tag=p.tag)) and \
+               os.path.isfile(depot.get_full_path(moby2.TODCuts, tod=tod, tag=params["tag_out"])):
+                pa = get_pathologies({'depot': p.depot,
+                                      'tag': p.tag}, tod=tod)
+                # get final cuts
+                pa.makeNewSelections()
+                # store pathologies crits
+                for k in all_keys:
+                    if "values" in pa.crit[k]:
+                        res[k].append(pa.crit[k]["values"])
+                    if "sel" in pa.crit[k]:
+                        res[f"{k}_sel"].append(pa.crit[k]["sel"])
+                # fix_tod_length(tod, pa.offsets)
+                res['sel'].append(pa.liveSel)
+                res['psel'].append(pa.preLiveSel)
+                resp, _, _, re_sel, _, _ = pa.getpWCalibration()
                 # get preselection
-                presel = pa.preLiveSel
-                psel.append(presel)
-                # get other tod info
-                tods.append(tod.info.name)
-                scanf.append(pa.scan_freq)
-                resp.append(re)
-                respSel.append(re_sel)
-                cal.append(calo.get_property("cal", det_uid=tod.det_uid, default=1)[1])
-                ctimes.append(tod.info.ctime)
-                alt.append(np.mean(tod.alt))
+                res['tods'].append(tod.info.name)
+                res['scanf'].append(pa.scan_freq)
+                res['resp'].append(resp)
+                res['resp_sel'].append(re_sel)
+                res['cal'].append(resp*pa.calData['ff'])
+                res['ctimes'].append(tod.info.ctime)
+                res['alt'].append(np.mean(tod.alt))
+                res['tod_sel'].append(True)
+            else:
+                res['tod_sel'].append(False)
 
         p.comm.Barrier()
         data = {}
-        data['name'] = util.allgatherv(tods, p.comm)
-        data['sel'] = util.allgatherv(sel, p.comm).T
-        data['psel'] = util.allgatherv(psel, p.comm).T
-        data["resp"] = util.allgatherv(resp, p.comm).T
-        data["scan_freq"] = util.allgatherv(scanf, p.comm)
-        data["respSel"] = util.allgatherv(respSel, p.comm).T
-        data["cal"] = util.allgatherv(cal, p.comm).T
-        data["ctime"] = util.allgatherv(ctimes, p.comm)
-        data["pwv"] = util.allgatherv(get_pwv(np.array(ctimes)), p.comm)
-        data["alt"] = util.allgatherv(alt, p.comm)
-        for k in list(criteria.keys()):
-            data[k] = util.allgatherv(np.array(criteria[k]), p.comm).T
-
+        data['name'] = util.allgatherv(res['tods'], p.comm)
+        data["scan_freq"] = util.allgatherv(res['scanf'], p.comm)
+        data['sel'] = util.allgatherv(res['sel'], p.comm).T
+        data['psel'] = util.allgatherv(res['psel'], p.comm).T
+        data["resp"] = util.allgatherv(res['resp'], p.comm).T
+        data["resp_sel"] = util.allgatherv(res['resp_sel'], p.comm).T
+        data["cal"] = util.allgatherv(res['cal'], p.comm).T
+        data["ctime"] = util.allgatherv(res['ctimes'], p.comm)
+        data["alt"] = util.allgatherv(res['alt'], p.comm)
+        data["pwv"] = util.allgatherv(get_pwv(np.array(res['ctimes'])),p.comm)
+        data['tod_sel'] = util.allgatherv(res['tod_sel'],p.comm)
+        for k in all_keys:
+            data[k] = util.allgatherv(np.array(res[k]), p.comm).T
+            ksel = f'{k}_sel'
+            data[ksel] = util.allgatherv(np.array(res[ksel]), p.comm).T
         if p.rank == 0:
             data["live"] = pa.liveCandidates
             data["dark"] = pa.origDark
-            data["ff"] = ff
-            data["stable"] = stable
-
+            data["ff"] = pa.calData['ff']
+            data["ff_sel"] = pa.calData['ffSel']
+            data["stable"] = pa.calData['stable']
+            array_data = moby2.scripting.get_array_data({
+                'instrument': 'actpol',
+                'array_name': p.i.ar,
+                'season': p.i.season
+            })
+            tes_sel = (array_data['nom_freq']== p.i.freq) * \
+                (array_data['det_type'] == 'tes')
+            data['tes_sel'] = tes_sel
             outfile = p.o.pickle_file
             print("Saving data: %s" % outfile)
-            f = open(outfile, 'wb')
-            p = pickle.Pickler(f,2)
-            p.dump(data)
-            f.close()
+            with open(outfile, 'wb') as f:
+                p = pickle.Pickler(f,2)
+                p.dump(data)
