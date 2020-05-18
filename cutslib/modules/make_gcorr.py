43f05e286530a1696328b87252866e7e4314de1e
Yilun Guan <zoom.aaron@gmail.com>
2020-05-18 17:47:00 -0400

    make_gcorr: make a dummy gain correction hdf file that enki accepts

---
+++ cutslib/modules/make_gcorr.py
@@ -0,0 +1,31 @@
+"""This module generates a gain correction required by enki,
+at this moment i will fill the list with default values"""
+
+from cutslib import Catalog
+import json
+
+class Module:
+    def __init__(self, config):
+        self.sel = json.loads(config.get('sel'))
+        self.outfile = config.get("outfile")
+        self.fill_value = config.getfloat("fill_value", None)
+
+    def run(self, p):
+        cat = Catalog()
+        cat.select(self.sel)
+        with open(self.outfile, "w") as f:
+            f.write(f"#                  tod_id band_id          cal\n")
+            for i, r in cat.data.iterrows():
+                todname = r.tod_name
+                if 'ar4' in 'todname':
+                    freqs = ['f150', 'f220']
+                elif 'ar5' in 'todname':
+                    freqs = ['f090', 'f150']
+                else:
+                    freqs = ['f090', 'f150']
+                # get calibration
+                if self.fill_value:
+                    cal = self.fill_value
+                for freq in freqs:
+                    f.write(f"{todname} {freq}  {cal}\n")
+        print(f"Written: {self.outfile}")
