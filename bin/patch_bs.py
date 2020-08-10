adef3edab7849aa180e8105d8f7bc9815850b8bf
Yilun Guan <zoom.aaron@gmail.com>
2020-08-10 10:04:05 -0400

    patch_bs: minor updates

--- bin/patch_bs.py
+++ bin/patch_bs.py
@@ -5,7 +5,7 @@ failed. It will find the previous timepoint that it didn't fail and use the
 measurements from that instead. This will recover roughly 5% of data.
 
 Example:
-> python patch_bs.py --season 2017 --array pa4 --depot /projects/ACT/yilung/depot/biasstep/ --tag calibration_20200808 --ignore-bias-orig -V
+> python patch_bs.py --season 2017 --array pa4 --depot /projects/ACT/yilung/depot/biasstep/ --tag calibration_20200808 -V --plot
 
 """
 import numpy as np
@@ -30,10 +30,11 @@ parser.add_argument('-D','--depot', help='depot for biassteps', default='/projec
 parser.add_argument('-S','--season', help='season to look at', required=True)
 parser.add_argument('-A','--array', help='array to look at', required=True)
 parser.add_argument('-T','--tag', help='tag to look at', default='calibration_20190802')
-parser.add_argument('--ignore-bias-orig', help='whether to ignore DET_BIAS_ORG line', action="store_true")
+parser.add_argument('--include-bias-orig', help='whether to include DET_BIAS_ORG line', action="store_true")
+parser.add_argument('--include-pxl-htr', help='whether to include PXL_HTR line', action="store_true")
 parser.add_argument('--plot', help='whether to produce debug plot', action="store_true")
 parser.add_argument('-F', '--force', help='whether to force overwriting cache', action="store_true")
-parser.add_argument('--cache-name', help='filename for caching', default="cache_bsinfo.pkl")
+parser.add_argument('--cache-name', help='filename for caching, no extension', default="cache_bsinfo")
 parser.add_argument('-V', '--verbose', help='more verbose', action="store_true")
 parser.add_argument('-W', '--write', help='write to file', action="store_true")
 args = parser.parse_args()
@@ -42,17 +43,17 @@ args = parser.parse_args()
 # utility functions #
 #####################
 
-def find_fix_map(bcs):
+def find_fix_map(ctimes, bcs):
     to_ = np.where(bcs==0)[0]
-    from_ = to_ - 1
-    dups = set(to_).intersection(from_)
-    while len(dups) > 0:
-        for dup in dups:
-            from_[from_==dup] -= 1
-        dups = set(to_).intersection(from_)
+    from_ = []
+    for i in to_:
+        idx = np.argsort(np.abs(ctimes - ctimes[i]))
+        j = 1
+        while idx[j] in to_: j += 1
+        from_.append(idx[j])
     return {'from':from_, 'to':to_}
 
-def patch_bs(from_path, to_path, bc, season, array, write):
+def patch_bs(from_path, to_path, bc, ad, write):
     """patch bias-step files
 
     Parameters
@@ -60,13 +61,12 @@ def patch_bs(from_path, to_path, bc, season, array, write):
     from_path: where to take bias-step from
     to_path: where to apply bias-step to
     bc: bias card to be patched (bc1, bc2, bc3)
-    season/array: to get the array data
+    ad: array_data dictionary from moby2
     write: flag to turn on actual writing
 
     """
     from_bs = MobyDict.from_file(from_path)
     to_bs = MobyDict.from_file(to_path)
-    ad = moby2.scripting.get_array_data({'season':season,'array_name':array})
     bc_dets = np.where(ad['bias_card'] == bc)[0]
     # find alive dets in the given bias card by intersection
     to_add = list(set(from_bs['det_uid']).intersection(bc_dets))
@@ -93,15 +93,19 @@ def patch_bs(from_path, to_path, bc, season, array, write):
 search_path = op.join(args.depot, args.season, args.tag, args.array, "*/*.cal")
 
 # get array data for bias_card info
-ad = moby2.scripting.get_array_data({'season':'2017','array_name':'ar4'})
+ad = moby2.scripting.get_array_data({'season':args.season,'array_name':args.array})
 
-# remove det_bias_org column from bias card 2 if necessary
-if args.ignore_bias_orig:
+# remove det_bias_org column from counting if necessary
+if not args.include_bias_orig:
     ad['bias_card'][ad['bias_name'] == 'DET_BIAS_ORG'] = 'none'
+# remove PXL_HTR column from counting if necessary
+if not args.include_pxl_htr:
+    ad['bias_card'][ad['bias_name'] == 'PXL_HTR'] = 'none'
 
-if not args.force and op.exists(args.cache_name):
+cache_name =f"{args.cache_name}_{args.season}_{args.array}.pkl"
+if not args.force and op.exists(cache_name):
     print("Use cached bias-step metadata")
-    with open(args.cache_name, "rb") as f:
+    with open(cache_name, "rb") as f:
         data = pickle.load(f)
     bc1s = data['bc1s']
     bc2s = data['bc2s']
@@ -141,7 +145,7 @@ else:
     ndets = np.asarray(ndets)[idx]
     fnames = np.asarray(fnames)[idx]
     # cache
-    with open(args.cache_name, "wb") as f:
+    with open(cache_name, "wb") as f:
         to_save = {
             'bc1s': bc1s, 'bc2s': bc2s, 'bc3s': bc3s,
             'ctimes': ctimes, 'ndets': ndets, 'fnames': fnames
@@ -162,7 +166,7 @@ if args.plot:
     plt.ylabel('# of dets')
     plt.xlabel('ctime')
     plt.legend(loc='upper left')
-    plt.savefig("debug_det_count.png")
+    plt.savefig(f"{args.season}_{args.array}_det_count.png")
     plt.close()
     # bias card failure plot
     plt.figure(figsize=(8,6))
@@ -172,14 +176,14 @@ if args.plot:
     plt.ylabel('Accumulation number of Biascard failure')
     plt.xlabel('ctime')
     plt.legend()
-    plt.savefig("debug_acc_bc_fail.png")
+    plt.savefig(f"{args.season}_{args.array}_acc_bc_fail.png")
     plt.close()
 
 # come up with fix table
 fixes = {
-    'bc1':find_fix_map(bc1s),
-    'bc2':find_fix_map(bc2s),
-    'bc3':find_fix_map(bc3s)
+    'bc1':find_fix_map(ctimes, bc1s),
+    'bc2':find_fix_map(ctimes, bc2s),
+    'bc3':find_fix_map(ctimes, bc3s)
 }
 fixes_df = []
 for bc in fixes:
@@ -193,17 +197,17 @@ for bc in fixes:
         })
 fixes_df = pd.DataFrame(fixes_df)
 outfile = f"fix_{args.season}_{args.array}.csv"
-fixes_df.to_csv(outfile, index=False)
+fixes_df.sort_values(by=['to_ctime']).to_csv(outfile, index=False, columns=['to_ctime','from_ctime','bc_fix'], sep=' ')
 
 # show the fix table
 print("Showing fix table")
 os.system(f"cat {outfile} | less")
 if input("proceed to fix? y/n ") == 'y':
-    print("Fixing...")
+    print("Start fixing...")
     if not args.write: print("write flag off: performing a dry-run")
     for i, row in fixes_df.iterrows():
         if args.verbose: print(f"{row['bc_fix']}: {row['from_ctime']} -> {row['to_ctime']}")
-        patch_bs(row['from_path'],row['to_path'],row['bc_fix'],args.season,args.array, args.write)
+        patch_bs(row['from_path'], row['to_path'], row['bc_fix'], ad, args.write)
 else:
     print("Fixing...")
     import time
