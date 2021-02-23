da9c6e9bdb8697921916484592f9ae61133b7412
Yilun Guan <zoom.aaron@gmail.com>
2021-02-23 16:23:46 -0500

    init: expose more variables in root scope

--- cutslib/__init__.py
+++ cutslib/__init__.py
@@ -1,6 +1,7 @@
 from .depot import SharedDepot, Depot
+from .release import Release
 from .catalog import Catalog
-from .load import load_tod, get_tes
+from .load import load_tod, get_tes, quick_transform
 from .season import SeasonStats
 from .glitch import CutsVector
 from .math import *
@@ -8,4 +9,4 @@ from .math import *
 # short cut for long import
 from moby2.util.database import TODList
 from moby2.util import MobyDict
-from moby2 import TODCuts
+from moby2 import TODCuts, Calibration
