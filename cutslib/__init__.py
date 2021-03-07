cc1e30f3f8766397197ab8ffbd0fd6ecb7f04aa4
Yilun Guan <zoom.aaron@gmail.com>
2021-03-07 14:40:15 -0500

    init: use moby2 CutsVector

--- cutslib/__init__.py
+++ cutslib/__init__.py
@@ -3,10 +3,12 @@ from .release import Release
 from .catalog import Catalog
 from .load import load_tod, get_tes, quick_transform
 from .season import SeasonStats
-from .glitch import CutsVector
+# from .glitch import CutsVector
 from .math import *
+from .visual import plot_tod
 
 # short cut for long import
 from moby2.util.database import TODList
 from moby2.util import MobyDict
 from moby2 import TODCuts, Calibration
+from moby2.tod.cuts import CutsVector
